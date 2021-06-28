dc137a1ed123feee5731dac9ff1c0c68cc0d7fc7
mahsa <mahsa.msv1991@gmail.com>
2021-06-28 13:55:24 +0000

    update eth_withdraw through bridge script

--- packages/eth_withdraw/scripts/exec_throughBridge.js
+++ packages/eth_withdraw/scripts/exec_throughBridge.js
@@ -1,7 +1,8 @@
 const { utils, providers, Wallet} = require("ethers");
 const { expect } = require("chai");
-const { Bridge} = require("arb-ts") 
+const { Bridge } = require("arb-ts") 
 const { parseEther } = utils
+
 
 require('dotenv').config();
 
@@ -18,33 +19,28 @@ const main = async () => {
 
     const l1Provider = new providers.JsonRpcProvider(process.env.L1RPC)
     const l2Provider = new providers.JsonRpcProvider(process.env.L2RPC)
-
     
     const preFundedWallet = new Wallet(walletPrivateKey, l1Provider)
     const l2Wallet = new Wallet(walletPrivateKey, l2Provider)
-    const L2initialbalance = await l2Wallet.getBalance()
 
-
-    const bridge = new Bridge(
-        process.env.L1EthErc20BridgeAddress,
-        process.env.L2ArbTokenBridgeAddress,
-        preFundedWallet,
-        l2Wallet
-    )
 
     const ethFromL2WithdrawAmount = parseEther('0.00001')
+    bridge = await Bridge.init(preFundedWallet, l2Wallet)
+
+    const preWithdrawalL2Balance = await bridge.getAndUpdateL2EthBalance()
+
+    const withdrawEthTx = await bridge.withdrawETH(ethFromL2WithdrawAmount, '')
+    const withdrawRec = await withdrawEthTx.wait()
+    expect(withdrawRec.status).to.equal(1)
     
-    
-    const tx = await bridge.withdrawETH(ethFromL2WithdrawAmount)
-    const rec = await tx.wait()
-    expect(rec.status).to.equal(1)
-    const withdrawEventData = (await bridge.getWithdrawalsInL2Transaction(rec))[0]
+    const withdrawEventData = (await bridge.getWithdrawalsInL2Transaction(withdrawRec))[0]
     expect(withdrawEventData).to.exist
+
 
     //Check to see if the balance is deducted after withdraw ETH
     wait()
     const L2EthBalance = await bridge.getAndUpdateL2EthBalance()
-    expect(L2EthBalance.lt(L2initialbalance)).to.be.true
+    expect(L2EthBalance.lt(preWithdrawalL2Balance)).to.be.true
 
 
 
