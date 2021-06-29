5692968706a777d1a1eab70ac8df869cac955a5d
mahsa <mahsa.msv1991@gmail.com>
2021-06-29 13:30:12 +0000

    token deposit using bridge script added

--- packages/token_deposit/scripts/exec_throughBridge.js
+++ packages/token_deposit/scripts/exec_throughBridge.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { BigNumber, utils, providers, Wallet } = require("ethers");
 const { ethers } = require("hardhat");
-const { Bridge, StandardArbERC20__factory  } = require("arb-ts") 
+const { Bridge  } = require("arb-ts"); 
 
 
 
@@ -15,31 +15,20 @@ const main = async () => {
     }
 
 
-   
-
     const infuraKey = process.env.INFURA_KEY
     if(!infuraKey) throw new Error("No INFURA_KEY set.")
 
     const walletPrivateKey = process.env.DEVNET_PRIVKEY
     if(!walletPrivateKey) throw new Error("No DEVNET_PRIVKEY set.")
 
-
     const l1Provider = new providers.JsonRpcProvider(process.env.L1RPC)
     const l2Provider = new providers.JsonRpcProvider(process.env.L2RPC)
 
-    
     const preFundedWallet = new Wallet(walletPrivateKey, l1Provider)
     const l2Wallet = new Wallet(walletPrivateKey, l2Provider)
 
-
-    const bridge = new Bridge(
-        process.env.L1EthErc20BridgeAddress,
-        process.env.L2ArbTokenBridgeAddress,
-        preFundedWallet,
-        l2Wallet
-    )
-
-      
+    const tokenDepositAmount = BigNumber.from(50)
+    bridge = await Bridge.init(preFundedWallet, l2Wallet)
 
  
     const L1DappToken = await (await ethers.getContractFactory('DappToken')).connect(preFundedWallet)
@@ -60,44 +49,59 @@ const main = async () => {
     expect(preFundedWallettBal && preFundedWallettBal.eq(BigNumber.from(1000000000000000))).to.be.true
 //************************************************************************************ 
     //approve token for bridge contract
-    const approvetx = await bridge.approveToken(erc20Address)
-    const rec = await approvetx.wait()
-    expect(rec.status).to.equal(1)
-    
+    const approveTx = await bridge.approveToken(erc20Address)
+    const approveRec = await approveTx.wait()
+    expect(approveRec.status).to.equal(1)
+
+
     const data2 = await bridge.getAndUpdateL1TokenData(erc20Address)
     const allowed = data2.ERC20 && data2.ERC20.allowed
     expect(allowed).to.be.true
 //************************************************************************************ 
 
-    //initial erc20 deposit txns — L1 and L2 — both succeed
-    const initialBridgeTokenBalance = await l1DappToken.balanceOf(bridge.ethERC20Bridge.address)
-    const l1gasPrice = utils.parseUnits('10', 'gwei')
-    const tokenDepositAmount = BigNumber.from(50)
+    const expectedL1GatewayAddress = await bridge.l1Bridge.getGatewayAddress(l1DappToken.address)
+    const initialBridgeTokenBalance = await l1DappToken.balanceOf(expectedL1GatewayAddress)
 
-    const depositTx = await bridge.deposit(
-        erc20Address,
-        tokenDepositAmount,
-        BigNumber.from(10000000000000),
-        BigNumber.from(0),
-        undefined,
-        { gasLimit: 210000, gasPrice: l1gasPrice }
+    const depositRes = await bridge.deposit(erc20Address,tokenDepositAmount)
+
+    const depositRec = await depositRes.wait()
+
+    const seqNumArr = await bridge.getInboxSeqNumFromContractTransaction(depositRec)
+    if (seqNumArr === undefined) {
+      throw new Error('no seq num')
+    }
+    expect(seqNumArr.length).to.exist
+    
+
+    const seqNum = seqNumArr[0]
+    const l2TxHash = await bridge.calculateL2TransactionHash(seqNum)
+    console.log('l2TxHash: ' + l2TxHash)
+
+    console.log('waiting for l2 transaction:')
+    const l2TxnRec = await l2Provider.waitForTransaction(
+      l2TxHash,
+      undefined,
+      1000 * 60 * 12
     )
-
-    const depositRec = await depositTx.wait()
-    await wait()
-
-    expect(depositRec.status).to.equal(1)
+    console.log('l2 transaction found!')
+    expect(l2TxnRec.status).to.equal(1)
 
 
 
-    const finalBridgeTokenBalance = await l1DappToken.balanceOf(bridge.ethERC20Bridge.address)
-    expect(initialBridgeTokenBalance.add(tokenDepositAmount).eq(finalBridgeTokenBalance))
 
-    const tokenDepositData = (await bridge.getDepositTokenEventData(depositRec))[0] 
-    const seqNum = tokenDepositData.seqNum
-    const l2RetryableHash = await bridge.calculateL2RetryableTransactionHash(seqNum)
-    const retryableReceipt = await l2Provider.waitForTransaction(l2RetryableHash) //TOOK a DAY!
-    expect(retryableReceipt.status).to.equal(1)
+    const finalBridgeTokenBalance = await l1DappToken.balanceOf(expectedL1GatewayAddress)
+
+    expect(initialBridgeTokenBalance.add(tokenDepositAmount).eq(finalBridgeTokenBalance)).to.be.true
+    const l2Data = await bridge.getAndUpdateL2TokenData(erc20Address)
+
+    const testWalletL2Balance = l2Data && l2Data.ERC20 && l2Data.ERC20.balance
+
+    expect(testWalletL2Balance && testWalletL2Balance.eq(tokenDepositAmount)).to .be.true
+
+
+
+
+
 
 
 
