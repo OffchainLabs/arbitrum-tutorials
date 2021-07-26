0e0e7be67da7ba966c1b67fcd2ed6b3d5fd58c30
dzgoldman <dzgoldman@wesleyan.edu>
2021-07-26 18:38:40 -0400

    cleanup

--- packages/address_table/hardhat.config.js
+++ packages/address_table/hardhat.config.js
@@ -1,14 +1,5 @@
 require('@nomiclabs/hardhat-waffle')
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
 module.exports = {
   solidity: '0.7.0',
   networks: {
