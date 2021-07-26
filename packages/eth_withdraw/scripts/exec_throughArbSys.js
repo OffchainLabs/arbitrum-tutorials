a96a9a82ea0b5e642c2cd17884e3c1ba76732cce
Mahsa Moosavi <mahsa.msv1991@gmail.com>
2021-07-26 13:50:21 -0400

    option 2 for Arbsys ethwithdraw added in the script

--- packages/eth_withdraw/scripts/exec_throughArbSys.js
+++ packages/eth_withdraw/scripts/exec_throughArbSys.js
@@ -49,25 +49,64 @@ const main = async () => {
     const arbSys = ArbSys__factory.connect(process.env.ARBSYS_ADDR, l2Wallet)
 
     /**
-    * Call the withdrawEth() frunction from the ArbSys contract
-    * Pass the l1Wallet.address as argument:  recipient address on L1
-    * Note that this is a convenience function, which is equivalent to calling sendTxToL1 with empty calldataForL1 (Option #2)
+    * As pointed in the readme, ETh withdrawals can be done by calling either of the following functions from the ArbSys contract:
+    * 1- withdrawEth(address destination) and 2-sendTxToL1(address destination, bytes calldata calldataForL1)
+    * Here we try both, feel free to choose between the two option and Don't forget the other one.
     */
 
-    const withdrawTx = await arbSys.withdrawEth(l1Wallet.address, { value: ethFromL2WithdrawAmount })
+
+    // // 1- Option #1: Withdrawing ETH using Arbsys.withdrawEth():
+
+    // /**
+    // * Call the withdrawEth() frunction from the ArbSys contract
+    // * Pass the l1Wallet.address as argument:  recipient address on L1
+    // * Note that this is a convenience function, which is equivalent to calling sendTxToL1 with empty calldataForL1 (Option #2)
+    // */
+
+    // const withdrawTx = await arbSys.withdrawEth(l1Wallet.address, { value: ethFromL2WithdrawAmount })
+    // const rec = await withdrawTx.wait()
+    // expect(rec.status).to.equal(1)
+    // console.warn('withdraw L2 receipt is:', rec.transactionHash)
+
+    // /**
+    // * Below, we run some proper checks to make sure the L2 side of the withdrawEth tx is confirmed
+    // * Here we get the L2ToL1Transaction event that was emitted by ArbSys.withdrawEth
+    // * If this event exists, it means the withfraw tx has been fully excuted
+    // */
+    // const withdrawEventData = (await bridge.getWithdrawalsInL2Transaction(rec))[0]
+    // expect(withdrawEventData).to.exist
+
+
+    // //Here we check if l2Wallet balance is prperly updated after withdraw ETH
+    // //Note that it takes ~1 week for l1Wallet balance to be updated (once the dispute period is over)
+    // wait()
+    // const l2WalletUpdatedEthBalance = await bridge.getL2EthBalance()
+    // expect(l2WalletUpdatedEthBalance.lt(l2WalletInitialEthBalance)).to.be.true
+    // console.log(
+    //     `your L2 balance is updated from ${l2WalletInitialEthBalance.toString()} to ${l2WalletUpdatedEthBalance.toString()} and your L1 balance will be updated after the dispute period!`
+    // )
+
+    // 2- Option #2: Withdrawing ETH using Arbsys.sendTxToL1():
+           
+    /**
+    * Call the sendTxToL1() frunction from the ArbSys contract
+    * Pass (1) the l1Wallet.address as argument:  recipient address on L1 plus (2) empty calldata for L1 contract call
+    * Note that this is a convenience function, which is equivalent to calling sendTxToL1 with empty calldataForL1 (Option #2)
+    */
+    const withdrawTx = await arbSys.sendTxToL1(l1Wallet.address, "0x", {value: ethFromL2WithdrawAmount});
     const rec = await withdrawTx.wait()
     expect(rec.status).to.equal(1)
     console.warn('withdraw L2 receipt is:', rec.transactionHash)
 
     /**
-    * Below, we run some proper checks to make sure the L2 side of the withdrawEth tx is confirmed
-    * Here we get the L2ToL1Transaction event that was emitted by ArbSys.withdrawEth
+    * Below, we run some proper checks to make sure the L2 side of the sendTxToL1 tx is confirmed
+    * Here we get the L2ToL1Transaction event that was emitted by ArbSys.sendTxToL1
     * If this event exists, it means the withfraw tx has been fully excuted
     */
     const withdrawEventData = (await bridge.getWithdrawalsInL2Transaction(rec))[0]
     expect(withdrawEventData).to.exist
-
-
+  
+  
     //Here we check if l2Wallet balance is prperly updated after withdraw ETH
     //Note that it takes ~1 week for l1Wallet balance to be updated (once the dispute period is over)
     wait()
@@ -76,15 +115,7 @@ const main = async () => {
     console.log(
         `your L2 balance is updated from ${l2WalletInitialEthBalance.toString()} to ${l2WalletUpdatedEthBalance.toString()} and your L1 balance will be updated after the dispute period!`
     )
-
-
-
-    //Use one of the following options to withdraw ETH from L2:
-
-    //Option 1-- sendTxToL1(address _destAddress, bytes calldata _calldataForL1):
-    //const withdrawEthTx = await arbSys.sendTxToL1(l2Wallet.address, "0x", {value: ethFromL2WithdrawAmount});
-    //const withdrawRec = await withdrawEthTx.wait()
-    //expect(withdrawRec.status).to.equal(1)
+  
 
 }
 
