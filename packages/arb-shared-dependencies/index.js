928bd2decc85bdd8b7db2b1501c3deaa41b678e8
dzgoldman <dzgoldman@wesleyan.edu>
2021-07-28 19:02:30 -0400

    add ridiculous arblog metho to shared deps

---
+++ packages/arb-shared-dependencies/index.js
@@ -0,0 +1,37 @@
+const wait = (ms = 0) => {
+  return new Promise(res => setTimeout(res, ms || 0))
+}
+
+const arbLog = async text => {
+  let str = '🔵'
+  for (let i = 0; i < 25; i++) {
+    await wait(40)
+    if (i == 12) {
+      str = `🔵${'🔵'.repeat(i)}🔵`
+    } else {
+      str = `🔵${' '.repeat(i * 2)}🔵`
+    }
+    while (str.length < 60) {
+      str = ` ${str} `
+    }
+    str = str
+
+    console.log(str)
+  }
+
+  console.log('Arbitrum Demo:', text)
+  await wait(2000)
+
+  console.log('Lets')
+  await wait(1000)
+
+  console.log('Go ➡️')
+  await wait(1000)
+  console.log('...🚀')
+  await wait(1000)
+  console.log('')
+}
+
+module.exports = {
+  arbLog,
+}
