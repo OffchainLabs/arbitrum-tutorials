d8e8120c4d86496ec549d1eee81423d79ec73664
Nick Pai <npai.nyc@gmail.com>
2021-10-13 12:27:49 -0400

    fix(outbox-execute): Fix typo in import of OutgoingMessageState

--- packages/outbox-execute/scripts/exec.js
+++ packages/outbox-execute/scripts/exec.js
@@ -1,5 +1,5 @@
 const { providers, Wallet } = require('ethers')
-const { Bridge, OutGoingMessageState } = require('arb-ts')
+const { Bridge, OutgoingMessageState } = require('arb-ts')
 const { arbLog, requireEnvVariables } = require('arb-shared-dependencies')
 
 require('dotenv').config()
@@ -81,7 +81,7 @@ module.exports = async txnHash => {
     `Waiting for message to be confirmed: Batchnumber: ${batchNumber}, IndexInBatch ${indexInBatch}`
   )
 
-  while (!outgoingMessageState === OutGoingMessageState.CONFIRMED) {
+  while (!outgoingMessageState === OutgoingMessageState.CONFIRMED) {
     await wait(1000 * 60)
     const outgoingMessageState = await bridge.getOutGoingMessageState(
       batchNumber,
@@ -89,17 +89,17 @@ module.exports = async txnHash => {
     )
 
     switch (outgoingMessageState) {
-      case OutGoingMessageState.NOT_FOUND: {
+      case OutgoingMessageState.NOT_FOUND: {
         console.log('Message not found; something strange and bad happened')
         process.exit(1)
         break
       }
-      case OutGoingMessageState.EXECUTED: {
+      case OutgoingMessageState.EXECUTED: {
         console.log(`Message already executed! Nothing else to do here`)
         process.exit(1)
         break
       }
-      case OutGoingMessageState.UNCONFIRMED: {
+      case OutgoingMessageState.UNCONFIRMED: {
         console.log(`Message not yet confirmed; we'll wait a bit and try again`)
         break
       }
