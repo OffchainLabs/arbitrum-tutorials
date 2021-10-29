57a9db1c02caa1f831c13e548ad1c987b3aedc1a
fred <fredlacs@gmail.com>
2021-10-29 19:44:22 +0100

    Add simplified smart contract setup

--- packages/arb-shared-dependencies/hardhat.config.js
+++ packages/arb-shared-dependencies/hardhat.config.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 module.exports = {
   solidity: {
     compilers: [
-     
       {
         version: "0.8.2",
         settings: {},
@@ -16,8 +15,10 @@ module.exports = {
         version: "0.6.12",
         settings: {},
       },
-      
-     
+      {
+        version: "0.6.11",
+        settings: {},
+      },
     ],
   },
   networks: {
@@ -25,12 +26,12 @@ module.exports = {
       gas: 2100000, 
       gasLimit: 0,
       url: process.env['L1RPC'] || '',
-      accounts: [process.env['DEVNET_PRIVKEY']],
+      accounts: process.env['DEVNET_PRIVKEY'] ? [process.env['DEVNET_PRIVKEY']] : [],
     
     },
     l2: {
       url: process.env['L2RPC'] || '',
-      accounts: [process.env['DEVNET_PRIVKEY']],
+      accounts: process.env['DEVNET_PRIVKEY'] ? [process.env['DEVNET_PRIVKEY']] : [],
     },
   },
 }
--- packages/custom-token/contracts/@openzeppelin/contracts-upgradeable/interfaces/draft-IERC2612Upgradeable.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-import "../token/ERC20/extensions/draft-IERC20PermitUpgradeable.sol";
-
-interface IERC2612Upgradeable is IERC20PermitUpgradeable {}
