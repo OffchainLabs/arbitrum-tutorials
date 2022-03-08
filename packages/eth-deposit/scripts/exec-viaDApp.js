44d621f815bffc4cda3fb87940253b4c8ff10497
mahsa <mahsa.msv1991@gmail.com>
2022-03-08 18:37:13 +0000

    eth deposit script (needs some edits still)

--- packages/eth-deposit/scripts/exec-viaDApp.js
+++ packages/eth-deposit/scripts/exec-viaDApp.js
@@ -1,6 +1,6 @@
 const { utils, providers, Wallet, BigNumber } = require('ethers')
 const { ethers } = require('hardhat')
-const { Bridge, networks } = require('arb-ts')
+const { EthBridger, getL2Network }  = require ('arb-ts')
 const { parseEther } = utils
 const { arbLog, requireEnvVariables } = require('arb-shared-dependencies')
 require('dotenv').config()
@@ -25,24 +25,24 @@ const l2Wallet = new Wallet(walletPrivateKey, l2Provider)
 const ethToL2DepositAmount = parseEther('0.0001')
 
 const main = async () => {
+  const wait = (ms = 0) => {
+    return new Promise(res => setTimeout(res, ms || 1000))}
   //await arbLog('Deposit Eth via arb-ts')
-  /**
-   * Use arb-ts networks file to retrieve the Rinkeby Inbox address
-   */
-  const l1ChainId = await l1Wallet.getChainId()
-  const l1Network = await networks[l1ChainId]
-  const inboxAddress = await l1Network.ethBridge.inbox;
 
   /**
-   * Use wallets to create an arb-ts bridge instance
-   * We'll use bridge for its convenience methods around checking txn statuses and ETH balances
+   * Use l2Network to create an arb-ts EthBridger instance
+   * We'll use EthBridger for its convenience methods around transferring ETH to L2
    */
-  const bridge = await Bridge.init(l1Wallet, l2Wallet)
+  const l2Network = await getL2Network(l2Provider)
+  const ethBridger = new EthBridger(l2Network)
+  const inboxAddress = ethBridger.l2Network.ethBridge.inbox
+
+
 
   /**
    * First, let's check the l2Wallet's initial ETH balance (before deposit txn)
    */
-  const l2WalletInitialEthBalance = await bridge.getL2EthBalance()
+  const l2WalletInitialEthBalance = await l2Wallet.getBalance()
 
   /**
    * Deploy the Deposit smart contract on L1
@@ -69,47 +69,19 @@ const main = async () => {
   const rec = await depositTx.wait()
   console.warn('deposit L1 receipt is:', rec.transactionHash)
 
-  /**
-   * With the transaction confirmed on L1, we now wait and check for the L2 side (i.e., balance credited to L2) to be confirmed as well.
-   * First, we get our txn's sequence number from the event logs (using a handy utility method)
-   * This sequence number uniquely identifies our L1-to-L2 message:
-   */
-  const seqNumArr = await bridge.getInboxSeqNumFromContractTransaction(rec)
-  if (seqNumArr === undefined) {
-    throw new Error('no seq num')
+  for (let i = 0; i < 60; i++) {
+    console.log('balance check attempt ' + (i + 1))
+    await wait(5000)
+    const testWalletL2EthBalance = await l2Wallet.getBalance()
+    if (l2WalletInitialEthBalance.add(ethToL2DepositAmount).eq(testWalletL2EthBalance)) {
+      console.log(`balance updated!  ${testWalletL2EthBalance.toString()}`)
+      //expect(true).to.be.true
+      return
+      break
+    }
   }
-  console.log('inbox sequence number is found!')
-  /**
-   * Note that a single txn could (in theory) trigger many l1-to-l2 messages; we know ours only triggered 1 tho.
-   */
-  const seqNum = seqNumArr[0]
 
-  /**
-   * Using the sequence number, we can deterministically predict what its corresponding L2 txn will be
-   */
-  const l2TxHash = await bridge.calculateL2TransactionHash(seqNum)
-  console.log('l2TxHash is: ' + l2TxHash)
-
-  /**
-   * ... and now we wait. Here we're waiting for the Sequencer to include the L2 message in its off-chain queue. The Sequencer should include it in under 10 minutes.
-   */
-  console.log('waiting for l2 transaction:')
-  const l2TxnRec = await l2Provider.waitForTransaction(
-    l2TxHash,
-    undefined,
-    1000 * 60 * 12
-  )
-  console.log(`L2 transaction found! ${l2TxnRec.transactionHash}`)
-
-  /**
-   * Our L2 balance should now be updated!
-   */
-
-  const l2WalletUpdatedEthBalance = await bridge.getL2EthBalance()
-
-  console.log(
-    `your L2 balance is updated from ${l2WalletInitialEthBalance.toString()} to ${l2WalletUpdatedEthBalance.toString()}`
-  )
+  
 }
 
 main()
