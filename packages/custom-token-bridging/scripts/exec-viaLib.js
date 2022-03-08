0d29ae532e61f3ed0dfe1a79f6e05696ca23367d
mahsa <mahsa.msv1991@gmail.com>
2022-03-08 16:01:47 +0000

    done w/ register custom token via Lib script (including inline comments)

--- packages/custom-token-bridging/scripts/exec-viaLib.js
+++ packages/custom-token-bridging/scripts/exec-viaLib.js
@@ -1,5 +1,5 @@
 const { providers, Wallet } = require('ethers')
-const { getL2Network } = require("arb-ts")
+const { getL2Network, L1ToL2MessageStatus } = require("arb-ts")
 const { arbLog, requireEnvVariables } = require('arb-shared-dependencies')
 const { AdminErc20Bridger } = require('arb-ts/dist/lib/assetBridger/erc20Bridger')
 const { expect } = require ('chai')
@@ -57,7 +57,6 @@ const main = async () => {
   await l2CustomToken.deployed()
   console.log(`custom token is deployed to L2 at ${l2CustomToken.address}`)
 
-  
   console.log("Registering custom token on L2:")
 
   /**
@@ -72,12 +71,34 @@ const main = async () => {
   
   const registerTokenRec = await registerTokenTx.wait()
   console.log(
-    `Registering token txn confirmed on L1! 🙌 ${registerTokenRec.transactionHash}`
+    `Registering token txn confirmed on L1! 🙌 L1 receipt is: ${registerTokenRec.transactionHash}`
   )
 
+  /**
+   * The L1 side is confirmed; now we listen and wait for the for the Sequencer to include the L2 side; we can do this by computing the expected txn hash of the L2 transaction.
+   * To compute this txn hash, we need our message's "sequence numbers", unique identifiers of each L1 to L2 message. We'll fetch them from the event logs with a helper method
+   */
   const l1ToL2Msgs = await registerTokenRec.getL1ToL2Messages(l2Provider)
-    expect(l1ToL2Msgs.length, 'Should be 2 messages.').to.eq(2)
- }
+  
+  /**
+   * In principle, a single L1 txn can trigger any number of L1-to-L2 messages (each with its own sequencer number). 
+   * In this case, the registerTokenOnL2 method created 2 L1-to-L2 messages; (1) one to set the L1 token to the Custom Gateway via the Router, and (2) another to set the L1 token to its L2 token address via the Generic-Custom Gateway
+   * Here, We check if both messages are redeemed on L2
+  */
+  expect(l1ToL2Msgs.length, 'Should be 2 messages.').to.eq(2)
+
+  const setTokenTx = await l1ToL2Msgs[0].waitForStatus()
+  expect(setTokenTx.status, 'Set token not redeemed.').to.eq(
+    L1ToL2MessageStatus.REDEEMED
+  )
+  const setGateways = await l1ToL2Msgs[1].waitForStatus()
+  expect(setGateways.status, 'Set gateways not redeemed.').to.eq(
+    L1ToL2MessageStatus.REDEEMED
+  )
+
+  console.log('Your custom token is now registered on our custom gateway 🥳  Go ahead and make the deposit!')
+
+}
 
 main()
   .then(() => process.exit(0))
