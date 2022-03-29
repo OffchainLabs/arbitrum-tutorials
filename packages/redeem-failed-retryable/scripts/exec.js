d1e619d903b5186cf52f8d997b4399c9add98056
mahsa <mahsa.msv1991@gmail.com>
2022-03-29 14:37:56 +0000

    edits on the script to consume arbitrum sdk

--- packages/redeem-failed-retryable/scripts/exec.js
+++ packages/redeem-failed-retryable/scripts/exec.js
@@ -1,13 +1,8 @@
 const { providers, Wallet } = require('ethers')
-const { Bridge } = require('arb-ts')
+const { L2TransactionReceipt, getL2Network, L1TransactionReceipt, L1ToL2MessageStatus } = require('@arbitrum/sdk')
 const { arbLog, requireEnvVariables } = require('arb-shared-dependencies')
-
 require('dotenv').config()
 requireEnvVariables(['DEVNET_PRIVKEY', 'L2RPC', 'L1RPC'])
-
-const wait = (ms = 0) => {
-  return new Promise(res => setTimeout(res, ms || 10000))
-}
 
 /**
  * Set up: instantiate L1 / L2 wallets connected to providers
@@ -38,55 +33,24 @@ module.exports = async txnHash => {
    throw new Error(`Hmm, ${txnHash} doesn't look like a txn hash...`)
 
   
-  /**
-  * Use wallets to create an arb-ts bridge instance
-  * We'll use bridge for its convenience methods around redeeimg a retryable ticket
-  */
-
-  const bridge = await Bridge.init(l1Wallet, l2Wallet)
+  const receipt = await l1Provider.getTransactionReceipt(txnHash)
+  const l1Receipt = new L1TransactionReceipt(receipt)
   
-  /**
-  * First, let's check if our tx has not been already executed on L2
-  */
-
-   console.log(`Ensuring retryable ticket hasn't been redeemed:`)
-
-  /**
-  * We get the L1 transaction from the tx hash provided 
-  * Using the L1 transaction, we then get our txn's sequence number from the event logs (using a handy utility method)
-  * This sequence number uniquely identifies our L1-to-L2 message
-  */
-
-  const l1Tx = await bridge.getL1Transaction(txnHash)
-  const inboxSeqNum = await bridge.getInboxSeqNumFromContractTransaction(l1Tx)
-  if (!inboxSeqNum) throw new Error('Inbox not triggered')
-
-  /**
-  * Using our txn's sequence number, we now get the hash of L2 side of retryable txn
-  */
-
-  const redemptionTxHash = await bridge.calculateL2RetryableTransactionHash( inboxSeqNum[0], l2Provider )
+  const message = await l1Receipt.getL1ToL2Message(l2Wallet)
+  const status = await message.waitForStatus()
+  if(status === L1ToL2MessageStatus.REDEEMED) {
+    console.log(`L2 retryable txn is already executed 🥳 ${message.l2TxHash}`)
+    } else {
+  console.log(`L2 retryable txn failed with status ${L1ToL2MessageStatus[status]}`)
+  }  
   
-  /**
-  * Now we'll see if the ticket has been redeemed or not  
-  */
-  const redemptionRec = await bridge.l2Provider.getTransactionReceipt(redemptionTxHash)
+  console.log(`Redeeming now 🥳 `)
 
-  if (redemptionRec && redemptionRec.status === 1) {
-    throw new Error(
-    `ticket has been already redeemed: ${redemptionTxHash}`
-    )
-  }
 
-  console.log(`Retryable ticket hasn't been redeemed yet, redeeming now 🥳 `)
-
-  /**
-  * Finally, we use bridge for redeeimg a retryable ticket
-  * our L1 transaction is passed as an argument
-  */
-  const res = await bridge.redeemRetryableTicket(l1Tx)
+  const res = await message.redeem()
   const rec = await res.wait()
-  console.log('The L2 side of your transaction is now execeuted 🥳 :', rec)
+
+  console.log('The L2 side of your transaction is now execeuted 🥳 :', message.l2TxHash)
   
 
 }
