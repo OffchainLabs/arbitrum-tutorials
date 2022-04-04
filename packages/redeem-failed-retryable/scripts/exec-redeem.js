8897525ab59a27e31493c6443ae562ba7310f38d
mahsa <mahsa.msv1991@gmail.com>
2022-04-04 16:37:33 +0000

    getting ticket status (another version)

--- packages/redeem-failed-retryable/scripts/exec-redeem.js
+++ packages/redeem-failed-retryable/scripts/exec-redeem.js
@@ -34,36 +34,46 @@ module.exports = async txnHash => {
   const receipt = await l1Provider.getTransactionReceipt(txnHash)
   const l1Receipt = new L1TransactionReceipt(receipt)
 
-
-  
   
   const message = await l1Receipt.getL1ToL2Message(l2Wallet)
+  const status = (await message.waitForStatus()).status
   const autoRedeemHash = message.autoRedeemId
   const autoRedeemRec = await l2Provider.getTransactionReceipt(autoRedeemHash)
 
+  switch(status)
+  {
+    case L1ToL2MessageStatus.REDEEMED: 
+      console.log(`L2 retryable txn is already executed 🥳 ${message.l2TxHash}`)
+    case L1ToL2MessageStatus.NOT_YET_CREATED:
+      console.log(`The retryable ticket has yet to be created`)
+    case L1ToL2MessageStatus.CREATION_FAILED:
+      console.log(`An attempt was made to create the retryable ticket, but it failed. This could be due to not enough submission cost being paid by the L1 transaction`)
+    case L1ToL2MessageStatus.FUNDS_DEPOSITED_ON_L2:
+      console.log(`The retryable ticket has been created but has not been redeemed. This could be due to the auto redeem failing, you can redeem it now:`)
+    case L1ToL2MessageStatus.EXPIRED:
+      console.log(`The message has either expired or has been canceled. It can no longer be redeemed.`)
+      
+  }
+  
+  
 
-  console.log(
-    autoRedeemRec.status
-  )
-
-
-switch(autoRedeemRec.status)
-{
-  case 1: 
-    console.log(`Your auto redeem reverted.`)
-  case 2:
-    console.log(`Auto redeem failed; hit congestion in the chain; you can redeem it now:"`)
-  case 8:
-    console.log(`auto redeem _TxResultCode_exceededTxGasLimit; you can redeem it now:"`)
-  case 10:
-    console.log(`auto redeem TxResultCode_belowMinimumTxGas; you can redeem it now:"`)
-  case 11:
-    console.log(`auto redeem TxResultCode_gasPriceTooLow; you can redeem it now:"`)
-  case 12:
-    console.log(`auto redeem TxResultCode_noGasForAutoRedeem; you can redeem it now:"`)  
-  default:
-    console.log(`auto redeem reverted; you can redeem it now:"`)  
-}
+  // switch(autoRedeemRec.status)
+  // {
+  //   case 1: 
+  //     console.log(`Your auto redeem reverted.`)
+  //   case 2:
+  //     console.log(`Auto redeem failed; hit congestion in the chain; you can redeem it now:`)
+  //   case 8:
+  //     console.log(`auto redeem _TxResultCode_exceededTxGasLimit; you can redeem it now:`)
+  //   case 10:
+  //     console.log(`auto redeem TxResultCode_belowMinimumTxGas; you can redeem it now:`)
+  //   case 11:
+  //     console.log(`auto redeem TxResultCode_gasPriceTooLow; you can redeem it now:`)
+  //   case 12:
+  //     console.log(`auto redeem TxResultCode_noGasForAutoRedeem; you can redeem it now:`)  
+  //   default:
+  //     console.log(`auto redeem reverted; you can redeem it now:`)  
+  // }
 
   // const status = await message.waitForStatus()
 
