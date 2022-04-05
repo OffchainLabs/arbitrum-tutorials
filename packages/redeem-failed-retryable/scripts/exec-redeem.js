e8a532ae5f4fbaf2fb9a49ef428d1178089e7257
mahsa <mahsa.msv1991@gmail.com>
2022-04-05 15:37:04 +0000

    adding the returnCode for retryables + correcting indentations

--- packages/redeem-failed-retryable/scripts/exec-redeem.js
+++ packages/redeem-failed-retryable/scripts/exec-redeem.js
@@ -1,13 +1,12 @@
 const { providers, Wallet } = require('ethers')
-const { L1TransactionReceipt, L1ToL2MessageStatus, L1ToL2Message, getL2Network } = require('@arbitrum/sdk')
+const { L1TransactionReceipt, L1ToL2MessageStatus, getRawArbTransactionReceipt } = require('@arbitrum/sdk')
 const { arbLog, requireEnvVariables } = require('arb-shared-dependencies')
 require('dotenv').config()
 requireEnvVariables(['DEVNET_PRIVKEY', 'L2RPC', 'L1RPC'])
 
 /**
- * Set up: instantiate the L2 wallet connected to provider
- */
-
+  * Set up: instantiate the L2 wallet connected to provider
+*/
 const walletPrivateKey = process.env.DEVNET_PRIVKEY
 
 const l1Provider = new providers.JsonRpcProvider(process.env.L1RPC)
@@ -15,11 +14,10 @@ const l2Provider = new providers.JsonRpcProvider(process.env.L2RPC)
 const l2Wallet = new Wallet(walletPrivateKey, l2Provider)
 
 module.exports = async txnHash => {
-
   await arbLog('Redeem A Failed Retryable Ticket')
 
   /**
-   / * We start with an L1 txn hash; this is transaction that triggers craeting a retryable ticket
+    * We start with an L1 txn hash; this is transaction that triggers craeting a retryable ticket
   */
   if (!txnHash)
     throw new Error(
@@ -29,24 +27,27 @@ module.exports = async txnHash => {
    throw new Error(`Hmm, ${txnHash} doesn't look like a txn hash...`)
 
   /**
-   * First, we check if our L1 to L2 message is redeemed on L2
+    * First, we check if our L1 to L2 message is redeemed on L2
   */
   const receipt = await l1Provider.getTransactionReceipt(txnHash)
   const l1Receipt = new L1TransactionReceipt(receipt)
 
   const message = await l1Receipt.getL1ToL2Message(l2Wallet)
   const status = (await message.waitForStatus()).status
-  const autoRedeemHash = message.autoRedeemId
-  const autoRedeemRec = await l2Provider.getTransactionReceipt(autoRedeemHash)
+
+  /**
+    * To check why out autoredeem has failed, we get the autoRedeemRec which has the returnCode field
+  */
+  const autoRedeemRec = await getRawArbTransactionReceipt(l2Provider,message.autoRedeemId)
 
   if(status === L1ToL2MessageStatus.REDEEMED) {
     console.log(`L2 retryable txn is already executed 🥳 ${message.l2TxHash}`)
-    } else {
-  console.log(`L2 retryable txn failed with status ${L1ToL2MessageStatus[status]}`)
+  } else {
+    console.log(`L2 retryable txn failed with status ${L1ToL2MessageStatus[status]}`)
   }  
   console.log(`Let's see why auto redeem failed before manullay redeeming our ticket:`)
 
-  switch(autoRedeemRec.status)
+  switch(autoRedeemRec.returnCode)
   {
     case 1: 
       console.log(`Your auto redeem reverted.`)
@@ -66,7 +67,7 @@ module.exports = async txnHash => {
 
   console.log(`Redeeming the ticket now 🥳`)
   /**
-   * We use the redeem() method from Arbitrum SDK to manually redeem our ticket
+    * We use the redeem() method from Arbitrum SDK to manually redeem our ticket
   */
   await message.redeem()
   console.log('The L2 side of your transaction is now execeuted 🥳 :', message.l2TxHash)
