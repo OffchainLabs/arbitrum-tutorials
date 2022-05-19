e02320246707a127d342d0ed5ee89f5a6d35e14b
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-05-19 14:17:15 +0800

    chore: add and config linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,30 +15,8 @@ module.exports = {
   },
   rules: {
     'prettier/prettier': 'error',
-    'no-unused-vars': 'warn',
+    'no-unused-vars': 'off',
     'prefer-const': [2, { destructuring: 'all' }],
     'object-curly-spacing': ['error', 'always'],
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      parser: '@typescript-eslint/parser',
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
-      ],
-      plugins: ['@typescript-eslint', 'prettier'],
-      rules: {
-        'no-empty-pattern': 'warn',
-        'prettier/prettier': ['error', { singleQuote: true }],
-        '@typescript-eslint/member-delimiter-style': ['off'],
-        '@typescript-eslint/no-explicit-any': ['warn'],
-        '@typescript-eslint/no-use-before-define': ['off'],
-        '@typescript-eslint/no-non-null-assertion': ['off'],
-        '@typescript-eslint/ban-ts-comment': ['warn'],
-      },
-    },
-  ],
 }
