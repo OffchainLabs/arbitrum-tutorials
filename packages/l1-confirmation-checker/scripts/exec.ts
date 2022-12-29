0046e8afc05c9aa83a509097607669301f389fb8
Jason-Wanxt <wanxutao123@gmail.com>
2022-12-29 14:49:28 +0800

    add inline comments and make functions return

--- packages/l1-confirmation-checker/scripts/exec.ts
+++ packages/l1-confirmation-checker/scripts/exec.ts
@@ -8,20 +8,34 @@ const l1Provider = new providers.JsonRpcProvider(process.env.L1RPC)
 const l2Provider = new providers.JsonRpcProvider(process.env.L2RPC)
 
 const main = async () => {
+    // check action param
     switch(args.action) {
         case "checkConfirmation":
-            await checkConfirmation(args.txHash, l2Provider)
+            const confirmations = await checkConfirmation(args.txHash, l2Provider)
+            if(confirmations.eq(0)) {
+                console.log("Block has not been submitted to l1 yet, please check it later...")
+            } else {
+                console.log(`Congrats! This block has been submitted to l1 for ${confirmations} blocks`)
+            }
             break
+
         case "findSubmissionTx":
             if(process.env.L1RPC === ''){
                 throw new Error("L1RPC not defined in env!")
             }
-            await findSubmissionTx(args.txHash, l1Provider, l2Provider)
+            const submissionTx = await findSubmissionTx(args.txHash, l1Provider, l2Provider)
+            if(submissionTx === "") {
+                console.log("No submission transaction found. (If event too old some rpc will discard it)")
+            } else {
+                console.log(`Submission transaction found: ${submissionTx}`)
+            }
             break
+
         default:
             console.log(`Unknown action: ${args.action}`)
     }
 }
+
 main()
 .then(() => process.exit(0))
 .catch(error => {
--- packages/l1-confirmation-checker/scripts/utils.ts
+++ packages/l1-confirmation-checker/scripts/utils.ts
@@ -5,7 +5,12 @@ import { NODE_INTERFACE_ADDRESS } from "@arbitrum/sdk/dist/lib/dataEntities/cons
 import { getL2Network } from "@arbitrum/sdk"
 import { providers, BigNumber } from "ethers";
 
-export const checkConfirmation = async (txHash: string,  l2Provider:providers.JsonRpcProvider) => {
+// This function will output  the number of L1 block confirmations the L1 batch-posting transaction has 
+// by a given L2 transaction.
+export const checkConfirmation = async (
+    txHash: string,
+    l2Provider:providers.JsonRpcProvider
+): Promise<BigNumber> => {
     // get related block hash
     let blockHash
     try{
@@ -17,24 +22,22 @@ export const checkConfirmation = async (txHash: string,  l2Provider:providers.Js
     const nodeInterface = NodeInterface__factory.connect( NODE_INTERFACE_ADDRESS, l2Provider)
     let result
 
+    // call nodeInterface precompile to get the number of L1 confirmations the sequencer batch has.
     try {
         result = await nodeInterface.functions.getL1Confirmations(blockHash)
     } catch(e){
         throw new Error("Check fail, reason: " + e)
     }
     
-    if(result.confirmations.eq(0)) {
-        console.log("Block has not been submitted to l1 yet, please check it later...")
-    } else {
-        console.log(`Congrats! This block has been submitted to l1 for ${result.confirmations} blocks`)
-    }
+    return result.confirmations  
 }
 
+// This function will output the L1 batch-posting transaction hash by a given L2 transaction hash.
 export const findSubmissionTx = async (
     txHash: string, 
     l1Provider: providers.JsonRpcProvider,
     l2Provider: providers.JsonRpcProvider
-) => {
+): Promise<string> => {
     // get related block number
     let blockNumber
     try{
@@ -47,7 +50,7 @@ export const findSubmissionTx = async (
     const nodeInterface = NodeInterface__factory.connect( NODE_INTERFACE_ADDRESS, l2Provider)
     const sequencer = SequencerInbox__factory.connect(l2Network.ethBridge.sequencerInbox, l1Provider)
     
-    // Get batch number first
+    // call nodeInterface precompile to get batch number first
     let result: BigNumber
     try {
         result = await (await nodeInterface.functions.findBatchContainingBlock(blockNumber)).batch
@@ -59,9 +62,11 @@ export const findSubmissionTx = async (
     // then get it emitted transaction hash
     const queryBatch = sequencer.filters.SequencerBatchDelivered(result)
     const emittedEvent = await sequencer.queryFilter(queryBatch)
+
+    // if no event emitted, just return ""
     if(emittedEvent.length === 0) {
-        console.log("No submission transaction found. (If event too old some rpc will discard it)")
+        return ""
     } else {
-        console.log(`Submission transaction found: ${emittedEvent[0].transactionHash}`)
+        return emittedEvent[0].transactionHash
     }
 }
