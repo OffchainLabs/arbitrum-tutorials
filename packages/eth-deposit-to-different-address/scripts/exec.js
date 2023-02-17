2529e0f922f1532977137fc4548a28a58fb6c0a0
TucksonDev <tucksondev@gmail.com>
2023-02-17 23:12:08 +0000

    Changed tutorial to use the new depositTo method

--- packages/eth-deposit-to-different-address/scripts/exec.js
+++ packages/eth-deposit-to-different-address/scripts/exec.js
@@ -1,16 +1,12 @@
 const { utils, providers, Wallet } = require('ethers')
-const { L1ToL2MessageStatus } = require('@arbitrum/sdk')
-const {
-  L1ToL2MessageCreator,
-} = require('@arbitrum/sdk/dist/lib/message/L1ToL2MessageCreator')
-const { ArbSdkError } = require('@arbitrum/sdk/dist/lib/dataEntities/errors')
+const { EthBridger, getL2Network, EthDepositStatus } = require('@arbitrum/sdk')
 const { parseEther } = utils
 const { arbLog, requireEnvVariables } = require('arb-shared-dependencies')
 require('dotenv').config()
 requireEnvVariables(['DEVNET_PRIVKEY', 'L1RPC', 'L2RPC'])
 
 /**
- * Set up: instantiate L1 / L2 wallets connected to providers
+ * Set up: Connect to L1/L2 providers and instantiate an L1 wallet
  */
 const walletPrivateKey = process.env.DEVNET_PRIVKEY
 
@@ -22,81 +18,63 @@ const l1Wallet = new Wallet(walletPrivateKey, l1Provider)
 /**
  * Set the destination address and amount to be deposited in L2 (in wei)
  */
-const destinationAddress = '0x2D98cBc6f944c4bD36EdfE9f98cd7CB57faEC8d6'
+const destAddress = '0x2D98cBc6f944c4bD36EdfE9f98cd7CB57faEC8d6'
 const ethToL2DepositAmount = parseEther('0.0001')
 
 const main = async () => {
   await arbLog('Deposit Eth via Arbitrum SDK on a different address')
 
   /**
-   * Use l1Wallet to create an Arbitrum SDK L1ToL2MessageCreator instance
-   * We'll use L1ToL2MessageCreator to easily create a custom Retryable Ticket
-   * to send ETH
+   * Use l2Network to create an Arbitrum SDK EthBridger instance
+   * We'll use EthBridger for its convenience methods around transferring ETH to L2
    */
-  const l1ToL2MessageCreator = new L1ToL2MessageCreator(l1Wallet)
+  const l2Network = await getL2Network(l2Provider)
+  const ethBridger = new EthBridger(l2Network)
 
   /**
    * First, let's check the ETH balance of the destination address
    */
   const destinationAddressInitialEthBalance = await l2Provider.getBalance(
-    destinationAddress
+    destAddress
   )
 
   /**
-   * Create Retryable Ticket
-   * This method will call Inbox's createRetryableTicket function which, as the name suggests, will create
-   * a Retryable Ticket with the information we send here. The method will also take care of all gas related
-   * parameters by calling the estimate functions.
-   * The parameters that we'll send to the method to create the transaction are:
-   * - from: L1 address transferring ETH
-   * - to: L2 address receiving ETH
-   * - l2CallValue: The amount of ETH being sent
-   * - callValueRefundAddress: L2 address receiving ETH (same as "to")
-   * - data: Any data we want to send over (we can set it as "0x")
+   * Transfer ether from L1 to L2
+   * This convenience method automatically queries for the retryable's max submission cost and forwards the appropriate amount to the specified address on L2
+   * Arguments required are:
+   * (1) amount: The amount of ETH to be transferred to L2
+   * (2) l1Signer: The L1 address transferring ETH to L2
+   * (3) l2Provider: An l2 provider
+   * (4) destinationAddress: The address where the ETH will be sent to
    */
-  const retryableTicketParams = {
-    from: l1Wallet.address,
-    to: destinationAddress,
-    l2CallValue: ethToL2DepositAmount,
-    callValueRefundAddress: destinationAddress,
-    data: '0x',
-  }
-  const l1SubmissionTx = await l1ToL2MessageCreator.createRetryableTicket(
-    retryableTicketParams,
-    l2Provider
-  )
-  const l1SubmissionTxReceipt = await l1SubmissionTx.wait()
-  console.log(
-    'L1 submission transaction receipt is:',
-    l1SubmissionTxReceipt.transactionHash
-  )
+  const depositTx = await ethBridger.depositTo({
+    amount: ethToL2DepositAmount,
+    l1Signer: l1Wallet,
+    l2Provider: l2Provider,
+    destinationAddress: destAddress,
+  })
+  const depositRec = await depositTx.wait()
+  console.warn('Deposit L1 receipt is:', depositRec.transactionHash)
 
   /**
    * With the transaction confirmed on L1, we now wait for the L2 side (i.e., balance credited to L2) to be confirmed as well.
-   * Here we're waiting for the Sequencer to include the L2 message in its off-chain queue. The Sequencer should include it in under 10 minutes.
-   * Technically, we create an L1ToL2Message SDK object which has the function waitForStatus that will help us find out when the retryable ticket has been created and redeemed.
+   * Here we're waiting for the Sequencer to include the L2 message in its off-chain queue. The Sequencer should include it in under 15 minutes.
    */
-  console.log('Now we wait for L2 side of the transaction to be executed ⏳')
-  const l1ToL2message = (
-    await l1SubmissionTxReceipt.getL1ToL2Messages(l2Provider)
-  )[0]
-  if (!l1ToL2message) {
-    throw new ArbSdkError('Unexpected missing L1ToL2 message.')
-  }
-  const retryableTicketResult = await l1ToL2message.waitForStatus()
+  console.warn('Now we wait for L2 side of the transaction to be executed ⏳')
+  const l2Result = await depositRec.waitForL2(l2Provider)
 
   /**
-   * The `status` property tells us if the l1 to l2 message was successful
+   * The `complete` boolean tells us if the l1 to l2 message was successful
    */
-  retryableTicketResult.status === L1ToL2MessageStatus.REDEEMED
+  l2Result.complete
     ? console.log(
         `L2 message successful: status: ${
-          L1ToL2MessageStatus[retryableTicketResult.status]
+          EthDepositStatus[await l2Result.message.status()]
         }`
       )
     : console.log(
         `L2 message failed: status ${
-          L1ToL2MessageStatus[retryableTicketResult.status]
+          EthDepositStatus[await l2Result.message.status()]
         }`
       )
 
@@ -104,7 +82,7 @@ const main = async () => {
    * Our destination address ETH balance should be updated now
    */
   const destinationAddressUpdatedEthBalance = await l2Provider.getBalance(
-    destinationAddress
+    destAddress
   )
   console.log(
     `L2 ETH balance of the destination address has been updated from ${destinationAddressInitialEthBalance.toString()} to ${destinationAddressUpdatedEthBalance.toString()}`
