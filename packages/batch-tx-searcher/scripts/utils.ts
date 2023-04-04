2fe78994b7876fdc27cb86fff476355eaa1517aa
Jason-Wanxt <wanxutao123@gmail.com>
2023-04-04 16:21:23 +0800

    add comments in code

--- packages/batch-tx-searcher/scripts/utils.ts
+++ packages/batch-tx-searcher/scripts/utils.ts
@@ -11,20 +11,21 @@ export interface BlockRange {
     end: number
 }
 
-const binarySearchTargetBlock =async (
+// Use binary search to find one of the target block.
+const binarySearchMatchedBlock =async (
     batchNum: BigNumber,
     latestBlockNum: number,
     nodeInterface: NodeInterface,
     l2network: L2Network
 ): Promise<number> => {
     const nitroGenesisBlock = l2network.nitroGenesisBlock
-
     let high = latestBlockNum;
     let low = nitroGenesisBlock;
 
     while (low < high) {
         const mid = Math.round((low + high)/2);
         const currentBatch = await nodeInterface.findBatchContainingBlock(mid)
+        //When we find one, we can return directly to reduce the rpc call load.
         if(currentBatch.eq(batchNum)) {
             return mid;
         } else if (currentBatch.gt(batchNum)) {
@@ -36,6 +37,7 @@ const binarySearchTargetBlock =async (
     return high == nitroGenesisBlock ? nitroGenesisBlock : high - 1;
 }
 
+//Given one of the matched block, then find all matched blocks.
 const findBlockRange =async (
     batchNum: BigNumber,
     blockNum: number,
@@ -45,29 +47,40 @@ const findBlockRange =async (
 ): Promise<BlockRange> => {
     let start = l2network.nitroGenesisBlock;
     let end = latestBlockNum;
-    for(let i = blockNum; i > 0; i-=25) {
+
+    /**
+     * First try to find all blocks whose block height lower than this blocknum.
+     * We should use a big number to reduce the search time, when find one not matched
+     * to this batch, we will stop search and then search back one by one to see which
+     * one is the last matched block. 15 was chosen based on the mean inequality and 
+     * our batches now contain an average of 200-250 blocks.
+     */
+    for(let i = blockNum; i > 0; i -= 15) {
         const cur = await nodeInterface.findBatchContainingBlock(i)
-        console.log(cur, " ", i)
         if(!(cur).eq(batchNum)) {
-            console.log(await nodeInterface.findBatchContainingBlock(i))
             start = i;
             break;
         }
     }
-    for(let i = start; i <= (start + 25); i++){
+    for(let i = start; i <= (start + 15); i++){
         if((await nodeInterface.findBatchContainingBlock(i)).eq(batchNum)) {
             console.log(await nodeInterface.findBatchContainingBlock(i))
             start = i;
             break;
         }
     }
-    for(let i = blockNum; i < latestBlockNum; i+=25) {
+
+    /**
+     * Then try to find all blocks whose block height higher than this blocknum use the
+     * same method.
+     */
+    for(let i = blockNum; i < latestBlockNum; i += 15) {
         if(!(await nodeInterface.findBatchContainingBlock(i)).eq(batchNum)) {
             end = i;
             break;
         }
     }
-    for(let i = end; i >= (end - 25); i--){
+    for(let i = end; i >= (end - 15); i--){
         if((await nodeInterface.findBatchContainingBlock(i)).eq(batchNum)) {
             console.log(await nodeInterface.findBatchContainingBlock(i))
             end = i;
@@ -77,15 +90,22 @@ const findBlockRange =async (
     return {start, end}
 }
 
+//Find all txns within the block range.
 export const getAllTxByBlockRange = async (
     blockRange: BlockRange,
     l2BatchProvider: providers.JsonRpcBatchProvider
 ): Promise<string[]> => {
+    //To reduce the rpc call load, we use JsonRpcBatchProvider way.
     const promises:Promise<providers.Block>[] = [];
+
+    //Add all the rpc call to the promises array.
     for(let i = blockRange.start; i <= blockRange.end; i++) {
         promises.push(l2BatchProvider.getBlock(i))
     }
+    //Call those promises rpc call in a single time.
     const blockResults = await Promise.all(promises);
+
+    //After we got those blocks information, we can extract all thx txns.
     let results: string[] = []
     for(let i = 0; i < blockResults.length; i++) {
         results.push(...blockResults[i].transactions)
@@ -95,10 +115,11 @@ export const getAllTxByBlockRange = async (
 
 
  /**
-   * This function will output the number of L1 block confirmations the L1 batch-posting transaction has 
-   * by a given L2 transaction
+   * This function is used to find the block range matched to a batch number.
+   * To achieve this, we should use binary search to find one of the matched Block by
+   * calling precompile nodeInterface.findBatchContainingBlock, then after we found one,
+   * we can search around this block to find all matched block.
    */
-// by a given L2 transaction.
 export const getBlockRangeByBatch = async (
     batchNum: BigNumber,
     l1Provider: providers.JsonRpcProvider,
@@ -117,8 +138,13 @@ export const getBlockRangeByBatch = async (
         throw new Error("Error: this is an invalid batch number. (Too low)")
     }
 
-    const oneOfTargetBlock = await binarySearchTargetBlock(batchNum, latestBlockNum, nodeInterface, l2Network)
-    const targetBlockRange = await findBlockRange(batchNum, oneOfTargetBlock, latestBlockNum, nodeInterface, l2Network)
+    //Then find one of the matched block.
+    console.log("Start querying nodeInterface to find one of matched Block...")
+    const oneOfMatchedBlock = await binarySearchMatchedBlock(batchNum, latestBlockNum, nodeInterface, l2Network)
+    
+    //Search around the block to find all matched blocks
+    console.log(`One of matched block founded: ${oneOfMatchedBlock}, now we will go and find all matched blocks...`)
+    const targetBlockRange = await findBlockRange(batchNum, oneOfMatchedBlock, latestBlockNum, nodeInterface, l2Network)
     
     return targetBlockRange
 }
